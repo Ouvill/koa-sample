fc93739e905d650de33adee10f29c075787829b3
Ouvill <contact@ouvill.net>
2020-02-16 02:27:40 +0900

    debug 追加

--- webpack.dev.ts
+++ webpack.dev.ts
@@ -2,7 +2,8 @@ import merge from "webpack-merge";
 import common from "./webpack.base";
 
 const config = merge(common, {
-  mode: "development"
+  mode: "development",
+  devtool: "source-map"
 });
 
 export default config;
