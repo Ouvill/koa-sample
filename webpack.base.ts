a8026ec55d167e0860cb72cce4ce09c3faab5863
Ouvill <contact@ouvill.net>
2020-02-17 02:05:21 +0900

    eslint 追加

--- webpack.base.ts
+++ webpack.base.ts
@@ -1,32 +1,32 @@
-import * as webpack from "webpack";
-import * as path from "path";
+import * as webpack from 'webpack';
+import * as path from 'path';
 
 const config: webpack.Configuration = {
-  entry: "./src/index.ts",
-  target: "node",
+  entry: './src/index.ts',
+  target: 'node',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "server.js"
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'server.js',
   },
   module: {
     rules: [
-      { test: /\.js$/, loader: "source-map-loader", enforce: "pre" },
+      {test: /\.js$/, loader: 'source-map-loader', enforce: 'pre'},
       {
         // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
         test: /\.tsx?$/,
         use: [
           {
-            loader: "ts-loader",
-            options: {}
-          }
+            loader: 'ts-loader',
+            options: {},
+          },
         ],
-        exclude: /node_modules/
-      }
-    ]
+        exclude: /node_modules/,
+      },
+    ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"]
-  }
+    extensions: ['.tsx', '.ts', '.js'],
+  },
 };
 
 export default config;
