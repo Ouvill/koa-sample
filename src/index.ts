0e68016f34c36b994efbe6ae98aac1d12371fd2a
Ouvill <contact@ouvill.net>
2020-02-16 17:52:06 +0900

    logger 変更

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,12 @@
 import Koa from "koa";
 import Router from "koa-router";
+import logger from "koa-logger";
 const app = new Koa();
 const router = new Router();
 
-// logger
-app.use(async (ctx, next) => {
-  await next();
-  const rt = ctx.response.get("X-Response-Time");
-  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
-});
-
-// x-response-time
-app.use(async (ctx, next) => {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  ctx.set("X-Response-Time", `${ms}ms`);
-});
+if (process.env.NODE_ENV !== "production") {
+  app.use(logger());
+}
 
 router.get("/", (ctx, next) => {
   ctx.body = "Hello World";
