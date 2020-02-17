bd0284c0f04581293f3a7191a41c2ba72aa3fa01
Ouvill <Ouvill@users.noreply.github.com>
2020-02-17 14:48:25 +0000

    Apply Prettier Change

--- __tests__/routes.test.ts
+++ __tests__/routes.test.ts
@@ -1,19 +1,19 @@
 //routes.test.js
-import request from 'supertest';
-import server from '../src/index';
+import request from "supertest";
+import server from "../src/index";
 beforeAll(async () => {
   // do something before anything else runs
-  console.log('Jest starting!');
+  console.log("Jest starting!");
 });
 // close the server after each test
 afterAll(() => {
   server.close();
-  console.log('server closed!');
+  console.log("server closed!");
 });
-describe('basic route tests', () => {
-  test('get home route GET /', async () => {
-    const response = await request(server).get('/');
+describe("basic route tests", () => {
+  test("get home route GET /", async () => {
+    const response = await request(server).get("/");
     expect(response.status).toEqual(200);
-    expect(response.text).toContain('Hello World!');
+    expect(response.text).toContain("Hello World!");
   });
 });
